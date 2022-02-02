e9896cc19e18941124bdb684343a4d3522c371cd
phsdem <pedrohsantosdemoura@gmail.com>
2022-02-02 09:55:56 -0400

    uptade

---
+++ assets/js/services.js
@@ -0,0 +1,12 @@
+let count = 1;
+
+setInterval( function(){
+    document.getElementById(`slide` + count).checked = true;
+    count++;
+
+
+    if( count > 5){
+        count = 1;
+    }
+    
+}, 3000 );
